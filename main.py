5b54dc4279caf74ffd4f3042258b63a5a81c5774
chodavon <chodavon61@gmail.com>
2024-12-14 18:38:41 +0700

    hi

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from behind import UserAuthApp  # Import the UserAuthApp class from 1.py
 if __name__ == "__main__":
     app = UserAuthApp()  # Create an instance of UserAuthApp
     app.mainloop()       # Start the Tkinter main loop
+    
